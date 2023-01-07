88084173381d0fce3c5d3e458538aed8388bf213
jribeiro <jribeiro@ufu.br>
2023-01-07 15:42:16 -0300

    DepartmentService

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,16 +1,26 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentListController implements Initializable {
+
+	private DepartmentService service;
+
 	@FXML
 	private TableView<Department> tableViewDepartment;
 	@FXML
@@ -20,14 +30,38 @@ public class DepartmentListController implements Initializable {
 	@FXML
 	private Button btNew;
 
+	private ObservableList<Department> obsList;
+
 	@FXML
 	public void onBtNewAction() {
 		System.out.println("onButtonAction");
 	}
 
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
+	}
+
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
+		initializeNodes();
 
 	}
 
+	private void initializeNodes() {
+		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+		// ajustando a tabela a vie Inteira
+		Stage stage = (Stage) Main.getMainScene().getWindow();
+		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
+		tableViewDepartment.prefWidthProperty().bind(stage.widthProperty());
+	}
+
+	public void updateTableView() {
+		if (service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		List<Department> list = service.findAll();
+		obsList = FXCollections.observableArrayList(list);
+		tableViewDepartment.setItems(obsList);
+	}
 }
--- src/module-info.java
+++ src/module-info.java
@@ -2,7 +2,9 @@ module javaFx_JDBC_Nelio_Curso {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires javafx.graphics;
-	
+	requires javafx.base;
+
 	opens application to javafx.graphics, javafx.fxml;
 	opens gui to javafx.graphics, javafx.fxml;
+	opens model.entities to javafx.graphics, javafx.fxml, javafx.base;
 }
