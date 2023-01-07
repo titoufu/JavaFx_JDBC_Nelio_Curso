3cb58782259e57f466dcc44d459de282f00853ec
jribeiro <jribeiro@ufu.br>
2023-01-07 18:24:57 -0300

    Adding database acess

--- src/module-info.java
+++ src/module-info.java
@@ -3,8 +3,12 @@ module javaFx_JDBC_Nelio_Curso {
 	requires javafx.fxml;
 	requires javafx.graphics;
 	requires javafx.base;
+	requires java.sql;
 
 	opens application to javafx.graphics, javafx.fxml;
 	opens gui to javafx.graphics, javafx.fxml;
 	opens model.entities to javafx.graphics, javafx.fxml, javafx.base;
+	opens model.dao to javafx.graphics, javafx.fxml, javafx.base;
+	opens db to javafx.graphics, javafx.fxml, javafx.base;
+	
 }
